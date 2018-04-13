feecb1d7696edd26ed4349030e867bd3b5ff3744
Jason Lin <infwonder@gmail.com>
2018-04-13 13:35:25 -0600

    learning Reflux more

--- demos/WalletReaderReFlux.js
+++ demos/WalletReaderReFlux.js
@@ -2,20 +2,13 @@
 
 // Cast-Iron sub-class and instance
 const Wallet = require( __dirname + '/core/Wallet.js');
-const WT = new Wallet( __dirname + '/.local/configs.json');
 
 // Reflux
 const Reflux = require('reflux');
 
 // variables
-//let tokenList = Object.keys(WT.TokenList);
-let tokenList = ['BCPT','ZIL','SNGLS','RHOC','MANA','LEND','DATA','GUP','LINK','OMG','QSP','REQ','RLC','ANT','BAT','OPT','SALT','WINGS','KIN','LST','GNT','GLA','DAT'];
-let stage = Promise.resolve();
-
-WT.setAccount("0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"); // Sorry Vitalik! 
-WT.hotGroups(tokenList);
-
-let statusUpdate = Reflux.createAction();
+let Actions = Reflux.createActions(['startUpdate', 'statusUpdate']);
+Actions.startUpdate.preEmit = (address) => { console.log(`-|| Account: ${address} ||-`) };
 
 class StatusStore extends Reflux.Store
 {
@@ -23,35 +16,46 @@ class StatusStore extends Reflux.Store
     {
         super();
         this.state = {'ETH': 0}; // <- set store's default state much like in React
+	this.tokenList = ['BCPT','ZIL','SNGLS','RHOC','DATA','GUP','LINK','OMG','QSP','REQ','RLC','ANT','BAT','OPT','SALT','WINGS','KIN','LST','GNT','GLA','DAT'];
+	this._count;
+	this._target;
+	this.WT = new Wallet( __dirname + '/.local/configs.json');
+    }
+
+    onStartUpdate(address)
+    {
 	this._count = 0;
+	this._target = this.tokenList.length;
+
+	this.WT.setAccount(address);
+	this.WT.hotGroups(this.tokenList);
+	
+	this.tokenList.map( (t) =>
+	{ 
+		Actions.statusUpdate({[t]: Number(this.WT.toEth(this.WT.addrTokenBalance(t)(this.WT.userWallet), this.WT.TokenList[t].decimals).toFixed(9))}); 
+	});
+	Actions.statusUpdate({'ETH': Number(this.WT.toEth(this.WT.addrEtherBalance(this.WT.userWallet), this.WT.TokenList['ETH'].decimals).toFixed(9))}); 
     }
 
     onStatusUpdate(status)
     {
 	this._count++;
 	this.state = {...this.state, ...status};
-	if (this._count == Object.keys(this.state).length) { 
-		console.log(status);
-		console.log('--------------');
+
+	//console.log(status);
+
+	if (this._count == this._target) { 
 		console.log(JSON.stringify(this.state, 0 ,2));
-		this._count = 0;
-	} else {
-		console.log(status);
+		console.log('--------------');
 	}
     }
 }
 
+// MAIN
 let s = new StatusStore;
+let address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"; // Sorry Vitalik! 
 
-s.listenTo(statusUpdate, s.onStatusUpdate); // listen to the statusUpdate action
+s.listenTo(Actions.startUpdate, s.onStartUpdate); // listen to the statusUpdate action
+s.listenTo(Actions.statusUpdate, s.onStatusUpdate); // listen to the statusUpdate action
 
-console.log(`-|| Account: ${WT.userWallet} ||-`);	
-
-tokenList.map( (t) =>
-{ 
-	statusUpdate({[t]: Number(WT.toEth(WT.addrTokenBalance(t)(WT.userWallet), WT.TokenList[t].decimals).toFixed(9))}); 
-});
-statusUpdate({'ETH': Number(WT.toEth(WT.addrEtherBalance(WT.userWallet), WT.TokenList['ETH'].decimals).toFixed(9))}); 
-
-
-process.exit(0);
+Actions.startUpdate(address);
