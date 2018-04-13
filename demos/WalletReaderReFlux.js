f029371a32bf0ad6bda1e60e66cf4cc560fb32ff
Jason Lin <infwonder@gmail.com>
2018-04-13 13:59:54 -0600

    learning Reflux even more

--- demos/WalletReaderReFlux.js
+++ demos/WalletReaderReFlux.js
@@ -6,26 +6,32 @@ const Wallet = require( __dirname + '/core/Wallet.js');
 // Reflux
 const Reflux = require('reflux');
 
-// variables
-let Actions = Reflux.createActions(['startUpdate', 'statusUpdate']);
+// Reflux Actions
+let Actions = Reflux.createActions(['startUpdate', 'statusUpdate', 'finishUpdate']);
 Actions.startUpdate.preEmit = (address) => { console.log(`-|| Account: ${address} ||-`) };
 
+// Store
 class StatusStore extends Reflux.Store
 {
-    constructor()
+    constructor(tokenList)
     {
         super();
         this.state = {'ETH': 0}; // <- set store's default state much like in React
-	this.tokenList = ['BCPT','ZIL','SNGLS','RHOC','DATA','GUP','LINK','OMG','QSP','REQ','RLC','ANT','BAT','OPT','SALT','WINGS','KIN','LST','GNT','GLA','DAT'];
+	this.tokenList = tokenList;
 	this._count;
 	this._target;
 	this.WT = new Wallet( __dirname + '/.local/configs.json');
+
+	// listenTo
+	this.listenTo(Actions.startUpdate, this.onStartUpdate); // listen to the statusUpdate action
+	this.listenTo(Actions.statusUpdate, this.onStatusUpdate); // listen to the statusUpdate action
+	this.listenTo(Actions.finishUpdate, this.onFinishUpdate); // listen to the statusUpdate action
     }
 
     onStartUpdate(address)
     {
 	this._count = 0;
-	this._target = this.tokenList.length;
+	this._target = this.tokenList.length + 1;
 
 	this.WT.setAccount(address);
 	this.WT.hotGroups(this.tokenList);
@@ -43,19 +49,20 @@ class StatusStore extends Reflux.Store
 	this.state = {...this.state, ...status};
 
 	//console.log(status);
+	if (this._count == this._target) Actions.finishUpdate();
+    }
 
-	if (this._count == this._target) { 
-		console.log(JSON.stringify(this.state, 0 ,2));
-		console.log('--------------');
-	}
+    onFinishUpdate() 
+    {
+	console.log(JSON.stringify(this.state, 0 ,2));
+	console.log(`--------------------`);
+	// we can perhaps store a copy of the state on disk?
     }
 }
 
 // MAIN
-let s = new StatusStore;
 let address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"; // Sorry Vitalik! 
-
-s.listenTo(Actions.startUpdate, s.onStartUpdate); // listen to the statusUpdate action
-s.listenTo(Actions.statusUpdate, s.onStatusUpdate); // listen to the statusUpdate action
+let tokenList = ['BCPT','ZIL','SNGLS','RHOC','DATA','GUP','LINK','OMG','QSP','REQ','RLC','ANT','BAT','OPT','SALT','WINGS','KIN','LST','GNT','GLA','DAT'];
+let s = new StatusStore(tokenList);
 
 Actions.startUpdate(address);
