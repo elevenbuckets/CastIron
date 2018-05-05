aa10d7c3088474cb840a67fa4f6cded21ff81b2c
Yu-Cheng Lin <infwonder@gmail.com>
2018-05-05 01:56:57 -0400

    Adding ENS on privnet as demo

---
+++ demos/pkgs/dapps/ENS/conditions/Sanity.js
@@ -0,0 +1,32 @@
+module.exports =
+{
+	ENS_startAuction_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+
+	ENS_newBid_sanity(addr, jobObj)
+	{
+		return true;
+	},
+
+	ENS_startAuctionsAndBid_sanity(addr, jobObj)
+	{
+		return true;
+	},
+
+	ENS_unsealBid_sanity(addr, jobObj)
+	{
+		return true;
+	},
+
+	ENS_finalizeAuction_sanity(addr, jobObj)
+	{
+		return true;
+	},
+
+	ENS_transfer_sanity(addr, jobObj)
+	{
+		return true;
+	}
+}
